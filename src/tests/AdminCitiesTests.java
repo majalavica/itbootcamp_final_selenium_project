881b1fe2767723e16e9b478fdf0a91c4e8617ab2
Marija Jovanovic <marija998.jovanovic@gmail.com>
2022-10-10 17:26:24 +0200

    .classpath changed

--- src/tests/AdminCitiesTests.java
+++ src/tests/AdminCitiesTests.java
@@ -1,10 +1,12 @@
 package tests;
 
+import org.testng.annotations.Test;
 
 public class AdminCitiesTests extends BasicTest {
 
-//	@Test(priority = 10) example
-//	public void AddingProductToTheCart() {
-//		driver.navigate().to(baseUrl + "/index.php?id_product=1&controller=product");
-//	}
+	@Test(priority = 10) 
+	public void AddingProductToTheCart() {
+		driver.navigate().to(baseUrl);
+	}
+	
 }
